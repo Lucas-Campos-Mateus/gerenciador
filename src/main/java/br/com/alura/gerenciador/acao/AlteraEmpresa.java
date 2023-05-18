101fb714858d3c688210bd73958751206033d7de
Lucas Campos <iphonelucas___@outlook.com>
2023-05-17 22:05:45 -0300

    Deixando o modelo MVC do servlet redondinho

--- src/main/java/br/com/alura/gerenciador/acao/AlteraEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/acao/AlteraEmpresa.java
@@ -1,4 +1,4 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -6,23 +6,20 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/alteraEmpresa")
-public class AlteraEmpresaServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+public class AlteraEmpresa implements Acao {
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
 		String nomeEmpresa = request.getParameter("nome");
 		String paramDataEmpresa = request.getParameter("data");
 		String paramId = request.getParameter("id");
 		Integer id = Integer.valueOf(paramId);
-		
+
 		Date dataAbertura = null;
 
 		try {
@@ -31,14 +28,13 @@ public class AlteraEmpresaServlet extends HttpServlet {
 		} catch (ParseException e) {
 			throw new ServletException(e);
 		}
-		
+
 		Banco banco = new Banco();
 		Empresa empresa = banco.buscaEmpresaPeloId(id);
-		
+
 		empresa.setNome(nomeEmpresa);
 		empresa.setDataAbertura(dataAbertura);
-		
-		response.sendRedirect("listaEmpresas");
-	}
 
+		return "redirect:entrada?acao=ListaEmpresas";
+	}
 }
---
+++ src/main/java/br/com/alura/gerenciador/acao/ListaEmpresas.java
@@ -0,0 +1,25 @@
+package br.com.alura.gerenciador.acao;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+public class ListaEmpresas implements Acao {
+
+	public String executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Banco banco = new Banco();
+		List<Empresa> list = banco.getEmpresas();
+
+		request.setAttribute("empresas", list);
+
+		return "forward:listaEmpresas.jsp";
+
+	}
+}
